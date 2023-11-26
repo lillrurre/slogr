3ee358286f34e6bfbd719e722e9a583551a93499
Erik Ehrström <erik.ehrstrom@gmail.com>
2023-11-26 19:06:44 +0200

    Add middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+	"time"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+
+	status      int
+	wroteHeader bool
+}
+
+func wrapResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	if rw.wroteHeader {
+		return
+	}
+	rw.status = code
+	rw.ResponseWriter.WriteHeader(code)
+	rw.wroteHeader = true
+}
+
+func RequestLogger(logger *slog.Logger) func(next http.Handler) http.Handler {
+	logger = logger.WithGroup("request")
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					logger.With("error", err).With("trace", debug.Stack()).Error("http panic")
+				}
+			}()
+
+			start := time.Now()
+			wrap := wrapResponseWriter(w)
+
+			next.ServeHTTP(wrap, r)
+
+			logger.With("duration", time.Since(start)).
+				With("path", r.URL.EscapedPath()).
+				With("method", r.Method).
+				With("status", wrap.status).
+				Info("http log")
+
+		}
+		return http.HandlerFunc(fn)
+	}
+}
